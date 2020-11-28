061f75d89636cf419fc5971f21c958fade6f1f5d
Johnny Lancaster <lancast001@gmail.com>
2020-11-28 12:42:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const timerOne = performance.now();
-myFunc(1000000000); // call whatever function you want to time here
+myFunc(); // call whatever function you want to time here
 const timerTwo = performance.now();
 
 // save results in variable
 const result = (timerTwo - timerOne) / 1000;
 
 // log results in the console if you wish
-console.log((timerTwo - timerOne) / 1000);
+console.log(result);
